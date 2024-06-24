d0eff9491ae2b91ac70f895b56a9fe655c922670
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-06-24 11:29:15 +0200

    fix: :bug: controller changed to include user_alias, session_state and uuid as well as endpoint to see if alias is taken and active

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -1,6 +1,7 @@
 // import { PrismaClient } from '@prisma/client'
 import { Elysia, t } from "elysia";
 import db from "../dbConnect";
+import { chatDisconnectPATCHRequest, chatNewMessagePOSTRequest } from "../utils/chatBodyPayloads";
 
 const chatController = new Elysia().group(
   "chat",
@@ -9,41 +10,79 @@ const chatController = new Elysia().group(
     app
       .get(
         "/",
-        async ({ error }) =>
-          await db.messageModel.findMany().catch(() => {
+        async ({ error, query }) => {
+            if (query.alias_exists !== undefined && query.alias_exists !== ""){
+                return db.messageModel.findMany({
+                    where: {
+                    AND: [{user_alias: {equals : query.alias_exists}},
+                        {session_state: {equals : "active"}}]
+                        
+                    }
+                }).then((results) => {
+                    if (results.length === 0){
+                        return {user_alias: query.alias_exists, exists : false}
+                    }
+                    console.log(results)
+                    return error(409, {user_alias: query.alias_exists, exists : true} )
+                }).catch(() => {
+                    return error(500, "Internal Server Error - Database Error");
+                })
+            }
+          return await db.messageModel.findMany().catch(() => {
             return error(500, "Internal Server Error - Database Error");
-          })
+          })}, {
+            query: t.Object({
+                alias_exists: t.Optional(t.String())
+            })}
       )
       .post(
         "/",
-        async ({ body, error }) =>
-          db.messageModel
+        async ({ body, error, headers }) => {
+            body.user_id = crypto.randomUUID();
+            if (body.user_alias === undefined || body.user_alias === ""){
+              body.user_alias = "Anonymous";
+            }
+          return await db.messageModel
             .create({
-              data: body,
+              data: body
             })
             .catch((er: Error) => {
-              if (
-                er
-                  .toString()
-                  .includes(`Duplicate message_id detected: ${body.message_id}`)
-              ) {
-                return error(
-                  409,
-                  `Duplicate message_id detected: ${body.message_id}`
-                );
-              }
+                const isTriggerErrorId = er.toString().includes(`Duplicate user_id detected: ${body.user_id}`);
+                const isTriggerErrorAlias = er.toString().includes(`${body.user_alias} is already taken. Please choose another alias.`);
+
+                if (isTriggerErrorId) {
+                    console.log('error id')
+                  return error(
+                    409,
+                    {message: `Duplicate user_id detected: ${body.user_id}`,
+                    user_id: body.user_id,
+                    error_on: "user_id"}
+                  );
+                }
+                if (isTriggerErrorAlias) {
+                    console.log('error alias')
+                    console.log(er)
+                    console.log(headers)
+                  return error(
+                    409,
+                    {message: `${body.user_alias} is already taken. Please choose another alias.`,
+                    user_alias: body.user_alias,
+                    error_on: "user_alias"}
+                  );
+                }
               return error(500, "Internal Server Error - Database Error");
-            }),
-        {
-          body: t.Object({
-            message_id: t.String({ maxLength: 36, minLength: 36 }),
-            message_text: t.String({
-              maxLength: 120,
-            }),
-            timestamp_sent: t.Date(),
-          }),
-        }
-      )
+            })}, {body: chatNewMessagePOSTRequest.body}
+        
+      ).patch('disconnect', async ({ body, error  }) => {
+        return await db.messageModel.update({
+          where: { user_id: body.user_id },
+          data: { session_state: "inactive" }
+        }).catch(() => {
+          return error(500, "Internal Server Error - Database Error");
+        })
+      }, {body: chatDisconnectPATCHRequest.body},
+    )
+      
 );
 
 export default chatController;
