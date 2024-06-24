13837ff2ef265940c95d7361e9fec6539112ef7c
Musa-Mabasa <musa14mabasa@gmail.com>
2024-06-24 12:21:41 +0200

    ✨ Implemented the searching endpoint

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,10 +1,67 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import base64 from "base-64";
 
-const searchController = new Elysia({ prefix: "/search" }).get(
-  "/",
-  () => {
-    return "Hello search";
-  }
-);
+const searchController = new Elysia({ prefix: "/search" })
+  .get(
+    "/",
+    async ({ query: { q } }) => {
+      try {
+        const payload = {
+          query: {
+            query_string: {
+              query: q,
+            },
+          },
+        };
 
-export default searchController
+        return fetch(`${process.env.ELASTIC_URL}/_search?pretty`, {
+          method: "post",
+          headers: {
+            Authorization: `Basic ${base64.encode(
+              `elastic:${process.env.ELASTIC_PASSWORD}`
+            )}`,
+            "Content-Type": "application/json",
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+          body: JSON.stringify(payload),
+        })
+          .then((resp) => resp.json())
+          .then((res) => {
+            const results = [];
+            for (const hit of res.hits.hits) {
+              results.push(hit._source);
+            }
+            return results;
+          })
+          .catch(() => {
+            throw new Error("Internal Server Error");
+          });
+      } catch (err) {
+        throw new Error("Internal Server Error");
+      }
+    },
+    {
+      query: t.Object({
+        q: t.String(),
+      }),
+    }
+  )
+  .get("/search/products", () => {})
+  .onError(({ code, error }) => {
+    console.log(error);
+
+    switch (code) {
+      case "VALIDATION":
+        return `Invalid query: ${
+          error.validator.Errors(error.value).First().message
+        }`;
+      case "INTERNAL_SERVER_ERROR":
+        return error.message;
+      case "NOT_FOUND":
+        return "Request not found";
+    }
+  });
+
+export default searchController;
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new Elysia()
   .use(chatController)
   .use(searchController)
   .get("/", () => "Hello Elysia")
-  .listen(process.env["PORT"] ?? 3000);
+  .listen(process.env["PORT"] || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
