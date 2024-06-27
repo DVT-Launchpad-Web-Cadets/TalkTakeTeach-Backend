1b6ca0e1f5083de3aea17a39ab074e0c05989eb6
Musa-Mabasa <musa14mabasa@gmail.com>
2024-06-27 17:11:13 +0200

    🎨 Added new migrations

--- prisma/migrations/20240621111549_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `user_alias` to the `tbchat` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "tbchat" ADD COLUMN     "session_state" VARCHAR(20) NOT NULL DEFAULT 'active'
