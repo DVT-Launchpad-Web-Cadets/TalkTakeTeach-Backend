6abd9a064fbf5f90cbe21560a8e22c0f0c455bc6
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-07-02 15:23:10 +0200

    refactor: :recycle: Added code for postgresql dialect

---
+++ src/dbConnect.ts
@@ -0,0 +1,17 @@
+import { Database } from './models/database'
+import {Pool} from 'pg'
+import { Kysely, PostgresDialect } from 'kysely'
+
+export const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: process.env.DB,
+    host: process.env.DATABASE_URL || 'localhost',
+    user: process.env.DATABASE_USER,
+    port: 5434,
+    max: 10,
+  })
+})
+
+export const db = new Kysely<Database>({
+  dialect,
+})
