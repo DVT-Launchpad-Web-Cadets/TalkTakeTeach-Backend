86985bd0f7abd2bff4f45fd3352b13da0cfd6692
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-07-02 15:23:53 +0200

    feat: :sparkles: created migrator file

---
+++ src/migrator.ts
@@ -0,0 +1,37 @@
+import { Kysely, PostgresDialect, Migrator, FileMigrationProvider } from "kysely"
+import { Pool } from "pg"
+import { Database } from "./models/database"
+import * as path from 'path'
+import { promises as fs } from 'fs'
+import { db } from "./dbConnect"
+
+async function migrateToLatest() {
+  
+    const migrator = new Migrator({
+      db,
+      provider: new FileMigrationProvider({
+        fs,
+        path,
+        migrationFolder: '/database',
+      }),
+    })
+    const { error, results } = await migrator.migrateToLatest()
+  
+    results?.forEach((it) => {
+      if (it.status === 'Success') {
+        console.log(`migration "${it.migrationName}" was executed successfully`)
+      } else if (it.status === 'Error') {
+        console.error(`failed to execute migration "${it.migrationName}"`)
+      }
+    })
+  
+    if (error) {
+      console.error('failed to migrate')
+      console.error(error)
+      process.exit(1)
+    }
+  
+    await db.destroy()
+  }
+  
+  migrateToLatest()
