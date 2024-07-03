1d5570ac7825a36ceb4c27734be6a033194ed7d6
Musa-Mabasa <musa14mabasa@gmail.com>
2024-07-03 09:42:41 +0200

    Merge remote-tracking branch 'origin/search-controller' into refactor-postgres

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -60,7 +60,6 @@ const searchController = new Elysia({ prefix: "/search" })
                 };
                 results.push(product);
               }
-              fetch(`${process.env.ELASTIC_URL}/percolator/_doc`);
               return results;
             })
             .catch((err) => {
@@ -109,7 +108,6 @@ const searchController = new Elysia({ prefix: "/search" })
                 };
                 results.push(product);
               }
-              fetch(`${process.env.ELASTIC_URL}/percolator/_doc`);
               return results;
             })
             .catch((err) => {
